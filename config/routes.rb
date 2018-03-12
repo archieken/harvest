91574ff3f381f1b169f4710082e65bd71975758f
archie kenwright <kenwright.aw@googlemail.com>
2018-03-12 16:39:31 +0100

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/show'
+
+  get 'order_lines/create'
+
+  get 'orders/index'
+
   devise_for :users
   root to: 'pages#home'
+    resources :users, only: [:new, :create, :edit, :update]
+    resources :orders, only: [:index]
+    resources :products, only: [:index, :show] do
+      resources :order_lines, only: [:create]
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
