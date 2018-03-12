ca60588d40f4f459628216caea5c69df7443b2c1
Joscha Koepke <joscha.koepke@gmail.com>
2018-03-12 17:50:50 +0100

    added category validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :products
+
+  validates :subcategory, presence: true, inclusion: ["vegetables", "fruits", "meat", "fish", "dairy & eggs", "bakery", "wine & drinks", "pantry", "deli"]
 end
