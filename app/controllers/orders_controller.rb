78c5916179906727549cf63b070ab9b59ae424c6
archie kenwright <kenwright.aw@googlemail.com>
2018-03-12 22:44:14 +0100

    changed order_line index method to only show orders belonging to the current user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
   def index
-    @order_lines = OrderLine.all
+    @order_lines = Order.where(user: current_user).find_by(status: "new").order_lines
   end
 end
