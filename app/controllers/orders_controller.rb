5945fd959fb1a2d32ff4d3456cb7de5e04d15867
alex ramos <alexinbcn19@gmail.com>
2018-03-13 14:55:56 +0100

    add pundit to orders model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,6 @@
 class OrdersController < ApplicationController
   def index
-    if (Order.where(user: current_user).nil? || Order.where(user: current_user).find_by(status: "new").nil?)
-      @order_lines = OrderLine.all
-    else
-      @order_lines = Order.where(user: current_user).find_by(status: "new").order_lines
-    end
+    @orders = policy_scope(Product)
+    authorize @orders
   end
 end
