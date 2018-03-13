498ab1ca6fc7ffa7a1055d21136e3a222412250f
alex ramos <alexinbcn19@gmail.com>
2018-03-13 14:55:17 +0100

    add pundit to orderLines model

--- app/controllers/order_lines_controller.rb
+++ app/controllers/order_lines_controller.rb
@@ -1,18 +1,30 @@
 class OrderLinesController < ApplicationController
+
+
+
   def create
     if (!current_user.orders.find_by(status:"new").blank?)
       order = current_user.orders.find_by(status:"new")
-      OrderLine.create!(product: Product.find(params[:product_id].to_i), quantity: 1, order: order)
+      @orderline = OrderLine.create!(product: Product.find(params[:product_id].to_i), quantity: 1, order: order)
+      authorize @orderline
       puts "existing"
     else
       order = Order.create(user: current_user)
       OrderLine.create(product: Product.find(params[:product_id].to_i), quantity: 1, order: order)
+      authorize @orderline
       puts "new"
     end
   end
 
+  def update
+    @orderline = OrderLine.update(params[:id])
+    authorize @orderline
+  end
+
 
   def destroy
-    OrderLine.destroy(params[:id])
+    @orderline = OrderLine.find(params[:id])
+    authorize @orderline
+    @orderline.destroy
   end
 end
