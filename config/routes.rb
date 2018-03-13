c7cd19dd1a232ceac536d8e059fca0ce970f13b1
archie kenwright <kenwright.aw@googlemail.com>
2018-03-13 11:40:41 +0100

    fixed order issue. added delete functionaility

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
     resources :products, only: [:index, :show] do
       resources :order_lines, only: [:create]
     end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :order_lines, only: [:destroy]
 end
