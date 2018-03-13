9a2c8be9fd2994b0867bd9f56dc78def8324e43a
Joscha Koepke <joscha.koepke@gmail.com>
2018-03-13 15:43:45 +0100

    added postcodechecker

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,38 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if !params[:address].blank?
+      pc = PostCodeChecker.new(address: params[:address])
+      if pc.save
+        distance_checker(pc)
+      end
+    end
   end
+
+  private
+
+  def distance_checker(pc)
+    if pc.latitude && pc.longitude
+      @check = pc.distance_from([-28.644033, 153.603776])
+      #output is given in meter
+        if @check < 20000
+          redirect_to products_path
+          #redirect to city
+        else
+          redirect_to root_path
+        #modal not currently in that city
+        end
+    else
+      #return location not found, popup
+        redirect_to root_path
+    end
+  end
+
+
+  # def postcodechecker_params
+  #   params.require(:post_code_checker).permit(:address)
+  # end
+
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312145643) do
+ActiveRecord::Schema.define(version: 20180313105254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,10 +34,19 @@ ActiveRecord::Schema.define(version: 20180312145643) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "amount_cents", default: 0, null: false
+    t.string "status", default: "new"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "post_code_checkers", force: :cascade do |t|
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "producers", force: :cascade do |t|
