c6196efbaa1952659b9f320dd2f3673315ebed4e
archie kenwright <kenwright.aw@googlemail.com>
2018-03-14 12:20:18 +0100

    add remove quantity basket

--- app/assets/javascripts/basketicon.js
+++ app/assets/javascripts/basketicon.js
@@ -2,3 +2,12 @@ function addToBasket() {
 var basket = document.getElementById("demo");
 basket.innerHTML = parseInt(basket.innerHTML) + 1;
 }
+
+function removeFromBasket() {
+var basket = document.getElementById("demo");
+if (parseInt(basket.innerHTML) > 0) {
+basket.innerHTML = parseInt(basket.innerHTML) - 1;
+}
+else { alert("cannot have a quantity of 0")}
+}
+
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
 
   #get 'orders/index'
 
+
+  get '/order_lines/:id/add', to: 'order_lines#add', as: 'add_order_lines'
+
+  get '/order_lines/:id/remove', to: 'order_lines#remove', as: 'remove_order_lines'
+
+
   devise_for :users
   root to: 'pages#home'
     resources :users, only: [:new, :create, :edit, :update]
@@ -15,7 +21,7 @@ Rails.application.routes.draw do
       resources :payments, only: [:new, :create]
     end
     resources :products, only: [:index, :show] do
-      resources :order_lines, only: [:create]
+      resources :order_lines, only: [:create, :update]
     end
     resources :order_lines, only: [:destroy]
 end
