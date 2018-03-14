309c3eadbdeae547623d1008a7b6ac35be227d4f
Joscha Koepke <joscha.koepke@gmail.com>
2018-03-14 11:55:59 +0100

    addednew controller

---
+++ app/controllers/postcodecheckers_controller.rb
@@ -0,0 +1,38 @@
+class PostcodecheckersController < ApplicationController
+
+skip_before_action :authenticate_user!, only: [:check_address]
+
+  def check_address
+    if !params[:address].blank?
+      pc = PostCodeChecker.new(address: params[:address])
+
+      if pc.save
+
+
+        distance_checker(pc)
+      end
+    end
+  end
+
+  private
+
+  def distance_checker(pc)
+    if pc.latitude && pc.longitude
+      @check = pc.distance_from([51.509720, -0.105144])
+      #output is given in km
+
+        if @check < 40
+          redirect_to products_path
+          #render json: {success: true}
+          #redirect to city
+        else
+          redirect_to root_path(invalid_code: true)
+        #modal not currently in that city
+        end
+    else
+      #return location not found, popup
+
+    end
+  end
+
+end
