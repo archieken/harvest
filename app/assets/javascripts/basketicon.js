1f8d1f48886972935ebbb3eddaa36dafae8ae695
archie kenwright <kenwright.aw@googlemail.com>
2018-03-14 15:50:00 +0100

    bask

--- app/assets/javascripts/basketicon.js
+++ app/assets/javascripts/basketicon.js
@@ -11,3 +11,18 @@ basket.innerHTML = parseInt(basket.innerHTML) - 1;
 else { alert("cannot have a quantity of 0")}
 }
 
+
+
+
+function addQuantity() {
+var quant = document.getElementByClass("qty");
+quant.innerHTML = parseInt(quant.innerHTML) + 1;
+}
+
+function removeQuantity() {
+var quant = document.getElementByClass("qty");
+if (parseInt(quant.innerHTML) > 0) {
+quant.innerHTML = parseInt(quant.innerHTML) - 1;
+}
+else { alert("cannot have a quantity of 0")}
+}
