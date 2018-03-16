9f98f8778878a1bdef2f5bfa30fff8c41f3b5dac
Joscha Koepke <joscha.koepke@gmail.com>
2018-03-16 13:26:02 +0100

    deletefunction

--- app/assets/javascripts/basketicon.js
+++ app/assets/javascripts/basketicon.js
@@ -15,18 +15,18 @@ else { alert("cannot have a quantity of 0")}
 
 
 function addQuantity() {
-var quant = document.getElementByClass("qty");
+var quant = document.getElementsByClass("qty");
 quant.innerHTML = parseInt(quant.innerHTML) + 1;
-var total = document.getElementByClass("product-card-price");
-total.innerHTML =  parseInt(document.getElementByClass("unitprice")) * parseInt(document.getElementByClass("qty"));
+var total = document.getElementsByClass("product-card-price");
+total.innerHTML =  parseInt(document.getElementsByClass("unitprice")) * parseInt(document.getElementByClass("qty"));
 }
 
 function removeQuantity() {
-var quant = document.getElementByClass("qty");
+var quant = document.getElementsByClass("qty");
 if (parseInt(quant.innerHTML) > 0) {
 quant.innerHTML = parseInt(quant.innerHTML) - 1;
-var total = document.getElementByClass("product-card-price");
-total.innerHTML =  parseInt(document.getElementByClass("unitprice")) * parseInt(document.getElementByClass("qty"));
+var total = document.getElementsByClass("product-card-price");
+total.innerHTML =  parseInt(document.getElementsByClass("unitprice")) * parseInt(document.getElementByClass("qty"));
 
 }
 else { alert("cannot have a quantity of 0")}
--- app/models/post_code_checker.rb
+++ app/models/post_code_checker.rb
@@ -2,6 +2,8 @@ class PostCodeChecker < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
+  validates :address, presence: true
+
   #  def self.return_address(lat, long)
   #   latitude = lat
   #   longitude = long
