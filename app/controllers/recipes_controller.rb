eac8ba488c537c906ee59da7b1b1182b495f69b3
Joscha Koepke <joscha.koepke@gmail.com>
2018-03-18 22:36:25 +0100

    added recipes show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,8 @@
 class RecipesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def show
+    @recipe = Recipe.find(params[:recipe_id])
+    authorize @recipe
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,4 @@ class Recipe < ApplicationRecord
   belongs_to :producer
   has_many :ingredients
   has_many :products, through: :ingredients
-
 end
