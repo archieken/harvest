9bb1a4fa5c41cad25c5511b8c16b282147b1bf67
Joscha Koepke <joscha.koepke@gmail.com>
2018-03-19 11:37:00 +0100

    added pundit poliy

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,12 @@
 class RecipesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_after_action :verify_authorized
+  skip_before_action :authenticate_user!, only: [:show]
 
   def show
     @recipe = Recipe.find(params[:recipe_id])
     authorize @recipe
   end
+
+
+
 end
