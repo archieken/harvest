3911d4df266c351e0b99fbc701282fb71fec858a
archie kenwright <kenwright.aw@googlemail.com>
2018-03-19 19:05:12 +0100

    modal products view

--- app/javascript/packs/product_page_modal.js
+++ app/javascript/packs/product_page_modal.js
@@ -1,11 +1,16 @@
-function toggleProduct(id) {
+// function toggleProduct(id) {
 
-  console.log(id)
-  // console.log(id.innerHTML);
-  // id = id.innerHTML
-  // document.querySelector(`#product-${id}`).classList.toggle('show-product');
-  // console.log("it works #2");
-}
+//   console.log(id)
+//   // console.log(id.innerHTML);
+//   // id = id.innerHTML
+//   // document.querySelector(`#product-${id}`).classList.toggle('show-product');
+//   // console.log("it works #2");
+// }
+
+// function toggleProduct2() {
+//   document.querySelector(`.product-modal`).classList.removeClass('show-product');
+//   console.log("it works #2");
+// }
 
 
 // var toggleProductLinks = document.querySelectorAll('.toggle-product');
