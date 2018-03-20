ca46de5284c491b117335ed3308e4e0df8aa5bf4
Joscha Koepke <joscha.koepke@gmail.com>
2018-03-20 15:29:56 +0100

    added schema, seeds, and table to product page!

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :producer
   belongs_to :category
   has_many :order_lines
+  has_one :nutrient
   has_many :orders, through: :order_lines
   monetize :price_cents
   has_many :ingredients
