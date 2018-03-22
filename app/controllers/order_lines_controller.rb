3fd64c79d4cd73d6500589197525ea2890f4c43a
archie kenwright <kenwright.aw@googlemail.com>
2018-03-22 09:34:22 +0100

    abc

--- app/controllers/order_lines_controller.rb
+++ app/controllers/order_lines_controller.rb
@@ -38,7 +38,6 @@ skip_before_action :authenticate_user!, except: :add_to_basket
 
    def remove
 
-    raise
     orderline = OrderLine.find(params[:id])
 
     order = orderline.order.amount - orderline.product.price
